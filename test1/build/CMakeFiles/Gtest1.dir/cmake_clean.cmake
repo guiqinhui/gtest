c68a81ad86d73d6b83ce02fc184cd99af82020ed
carmelogui <carmelogui@tencent.com>
2019-07-20 16:09:09 +0800

    add gtest/test1

---
+++ test1/build/CMakeFiles/Gtest1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Gtest1.dir/abs.cc.o"
+  "Gtest1.pdb"
+  "Gtest1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Gtest1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ test1/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/code/gtest/test1
+# Build directory: /mnt/code/gtest/test1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(abstest "Gtest1")
